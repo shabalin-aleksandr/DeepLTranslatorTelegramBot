b471dfae7b59ecb7dd41bb24456d4ac9f961dbf2
Doberman786 <sasha19822010@gmail.com>
2023-03-16 00:39:08 +0100

    Started a project

---
+++ src/main/java/com/telegrambot/deepl/DeeplTranslatorTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.telegrambot.deepl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeplTranslatorTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeeplTranslatorTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/telegrambot/deepl/DeeplTranslatorTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegrambot.deepl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeeplTranslatorTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
